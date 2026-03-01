7cd7f69a7e9ecfee5725a09b610d81369d0daaa4
Saksham Yadav <sakshamyadav260705@gmail.com>
2026-03-01 20:08:51 +0530

    update more info controlflow

--- intermediate/05_controlflow.js
+++ intermediate/05_controlflow.js
@@ -71,3 +71,52 @@ switch (month){
 }
 
 // if break is removed then all the condition except default get executed
+
+
+const userEmail ="saksham@gami.com"
+if(userEmail){
+    console.log("GOT USER Email")
+}
+else{
+    console.log("Don't havr user Email");
+}
+/*
+falsy values -> which to assume as false;
+
+false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+truthy values
+
+"0",'false'," ",[],{},function(){}
+*/
+if(userEmail.length ===0){
+    console.log("Objest is empty");
+
+}
+const emptyObj={}
+
+if(Object.keys(emptyObj).length===0){
+    console.log("object is empty");
+}
+
+// false == 0    =>true
+// false == ' '  =>true
+// 0 ==' '       =>true
+
+// Nullish Coalescing Operator (??):null undefined
+
+let val1;
+// val1=5??10
+// val1=null ?? 10
+val1=undefined ?? 15
+val2=null??10??15;
+
+
+console.log(val2);
+console.log(val1);
+
+// terniaary Operator
+
+// conditon  ? true :false 
+const iceTeaPrice =300
+iceTeaPrice <=80 ? console.log("less than 80") :console.log("more than 80");
