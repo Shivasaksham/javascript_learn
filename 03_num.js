5299b1923bac79667e873f0ad89f48652f55baa9
Saksham Yadav <sakshamyadav260705@gmail.com>
2026-03-01 16:45:31 +0530

    about num and random function for range

---
+++ 03_num.js
@@ -0,0 +1,37 @@
+// const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+
+
+// const otherNumber=1123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++MATHS++++++++++++++++++
+
+
+console.log(Math);
+
+console.log(Math.abs(-2));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.5));
+console.log(Math.floor(4.5));
+console.log(Math.sqrt(25));
+console.log(Math.min(3,4,6,3));
+console.log(Math.max(3,4,6,3));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) +1);
+
+// const min =10;
+// const max=20;
+
+// console.log(Math.floor(Math.random() *(max-min+1))+min);
+
+// improtant for range fixing 
